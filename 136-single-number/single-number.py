d1b34e64e24ba27abd31045b9e76031a062c1471
Divesh Singla <diveshsingla9@gmail.com>
2024-03-22 06:38:50 +0530

    Time: 106 ms (80.28%) | Memory: 19.2 MB (35.05%) - LeetSync

---
+++ 136-single-number/single-number.py
@@ -0,0 +1,15 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        result = 0
+        for x in nums:
+            
+            result ^= x
+            # print(result)
+        return result
+        
+
+
+        
+        
+
+                
