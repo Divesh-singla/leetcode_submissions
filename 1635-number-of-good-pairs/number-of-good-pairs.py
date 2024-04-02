7606ce5c223611d28a2f10f38f127a7aeb3fee12
Divesh Singla <diveshsingla9@gmail.com>
2024-04-02 15:21:45 +0530

    Time: 44 ms (11.20%) | Memory: 16.5 MB (40.56%) - LeetSync

--- 1635-number-of-good-pairs/number-of-good-pairs.py
+++ 1635-number-of-good-pairs/number-of-good-pairs.py
@@ -1,13 +1,10 @@
 class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
-    # # BRUTE FORCE APPROACH
         result = 0
         for x in range(len(nums)):
             for y in range(x):
                 if nums[x] == nums[y]:
                     result+=1
         return result
-
-    # # APPROACH 2
 
         
