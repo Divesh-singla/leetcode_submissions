0e758a798815aaee58eb1f1ea08c31230efe6337
Divesh Singla <diveshsingla9@gmail.com>
2024-04-04 19:40:17 +0530

    Time: 69 ms (9.14%) | Memory: 16.6 MB (61.26%) - LeetSync

--- 2756-buy-two-chocolates/buy-two-chocolates.py
+++ 2756-buy-two-chocolates/buy-two-chocolates.py
@@ -1,16 +1,25 @@
 class Solution:
     def buyChoco(self, prices: List[int], money: int) -> int:
-        result = 0
-        prices.sort()
-        if prices[0]<money:
+    ### first approach
+        # result = 0
+        # prices.sort()
+        # if prices[0]<money:
 
-            result+=prices[0]
-            result+=prices[1]
-            if (money-result)>=0:
-                return money-result
-            else:
-                return money
-        else:
+        #     result+=prices[0]
+        #     result+=prices[1]
+        #     if (money-result)>=0:
+        #         return money-result
+        #     else:
+        #         return money
+        # else:
+        #     return money
+
+    ### second approach 
+        prices.sort()
+
+        if prices[0] + prices[1] > money:
             return money
+        else:
+            return money - (prices[0] + prices[1])
 
         
