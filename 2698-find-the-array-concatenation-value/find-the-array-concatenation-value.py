3fc0570ea96202bec935e301479cef7295a8b2db
Divesh Singla <diveshsingla9@gmail.com>
2024-04-08 05:14:28 +0530

    Time: 57 ms (32.91%) | Memory: 16.6 MB (86.25%) - LeetSync

--- 2698-find-the-array-concatenation-value/find-the-array-concatenation-value.py
+++ 2698-find-the-array-concatenation-value/find-the-array-concatenation-value.py
@@ -2,13 +2,28 @@ class Solution:
     def findTheArrayConcVal(self, nums: List[int]) -> int:
         result = 0
         
-        while (nums)!=[]:
-            if len(nums)>1:
-                result+=int(f'{nums[0]}{nums[-1]}')
-                del nums[0]
-                del nums[-1]
+        # while (nums)!=[]:
+        #     if len(nums)>1:
+        #         result+=int(f'{nums[0]}{nums[-1]}')
+        #         del nums[0]
+        #         del nums[-1]
 
-            else:
-                result+=nums[0]
-                del nums[0]
+        #     else:
+        #         result+=nums[0]
+        #         del nums[0]
+        # return result
+
+## approach 2
+        i = 0
+        j = len(nums)-1
+        result = 0
+
+        while i<=j:
+            if i==j:
+                result+=nums[i]
+                break
+            result+=int(f'{nums[i]}{nums[j]}')
+            i+=1
+            j-=1
         return result
+
