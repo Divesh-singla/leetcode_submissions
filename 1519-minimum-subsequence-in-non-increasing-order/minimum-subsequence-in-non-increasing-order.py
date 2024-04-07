c7b08a2730a3a679c4c73b64bfa856f642627453
Divesh Singla <diveshsingla9@gmail.com>
2024-04-08 04:49:21 +0530

    Time: 59 ms (68.00%) | Memory: 16.6 MB (84.42%) - LeetSync

--- 1519-minimum-subsequence-in-non-increasing-order/minimum-subsequence-in-non-increasing-order.py
+++ 1519-minimum-subsequence-in-non-increasing-order/minimum-subsequence-in-non-increasing-order.py
@@ -1,18 +1,11 @@
 class Solution:
     def minSubsequence(self, nums: List[int]) -> List[int]:
         nums.sort(reverse=True)
-        for x in range(1,len(nums)+1):
-            if sum(nums[:x])>sum(nums[x:]):
-                return nums[:x]
-# class Solution:
-#     def minSubsequence(self, nums: List[int]) -> List[int]:
-#         nums.sort(reverse=True)
-#         total_sum = sum(nums)
-#         subsequence_sum = 0
-#         subsequence = []
-
-#         for num in nums:
-#             subsequence_sum += num
-#             subsequence.append(num)
-#             if subsequence_sum > (total_sum - subsequence_sum):
-#                 return subsequence
+        subArray = []
+        subResult=0
+        numsSum = sum(nums)
+        for x in nums:
+            subArray.append(x)
+            subResult+=x
+            if subResult>(numsSum-subResult):
+                return subArray
