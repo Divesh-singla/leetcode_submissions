4317efcf327fdc3089518cb78f603caedfe51aae
Divesh Singla <diveshsingla9@gmail.com>
2024-04-14 19:15:25 +0530

    Time: 75 ms (5.10%) | Memory: 17.1 MB (47.04%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,10 +1,30 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        finalStr = ""
-        for x in s.lower():
-            if ord(x) in range(97,123) or ord(x) in range(48,58) :
-                finalStr+=x
+## APPROACH ONE-----------------------------------------------------------------------
+        # finalStr = ""
+        # for x in s.lower():
+        #     if ord(x) in range(97,123) or ord(x) in range(48,58) :
+        #         finalStr+=x
 
 
-        # print(finalStr)
-        return(finalStr == finalStr[::-1])
+        # # print(finalStr)
+        # return(finalStr == finalStr[::-1])
+
+## APPROACH TWO-----------------------------------------------------------------------
+        i=0
+        j=len(s)-1
+
+        while i<j:
+            print(s[i],s[j])
+            if s[i].isalnum()==False:
+                i+=1
+            elif s[j].isalnum()==False:
+                j-=1
+            elif s[i].lower()!=s[j].lower():
+                return False
+            elif s[i].lower()==s[j].lower():
+                i+=1
+                j-=1
+        return True
+            
+
