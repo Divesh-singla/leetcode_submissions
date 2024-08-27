eb4b2f6cdec3acd29d3a569afa287183b3787800
Divesh Singla <diveshsingla9@gmail.com>
2024-08-27 20:09:34 +0530

    Time: 139 ms (56.57%) | Memory: 18.2 MB (79.82%) - LeetSync

--- 2470-removing-stars-from-a-string/removing-stars-from-a-string.py
+++ 2470-removing-stars-from-a-string/removing-stars-from-a-string.py
@@ -2,12 +2,11 @@ class Solution:
     def removeStars(self, s: str) -> str:
         from collections import deque
         stack = deque()
-        
+
         for x in s:
-            if x == "*":
-                if len(stack) != 0:
-                    stack.pop()
+            if x=="*":
+                stack.pop()
             else:
                 stack.append(x)
 
-        return "".join(stack)
+        return ("".join(stack))
